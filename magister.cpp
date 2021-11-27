5c15517b2984b0e3ebb48a789c2de4c7fc25eefc
MagisterYodaa <94544814+MagisterYodaa@users.noreply.github.com>
2021-11-27 08:43:55 +0300

    Update magister.cpp

--- magister.cpp
+++ magister.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
-
+using std::cout;
+using std::endl;
 
 int main() {
-	setlocale(LC_ALL, "Russian");
-	std::cout << "Magic" << std::endl;
-	std::cout << "Дамблдор" << std::endl;
-	std::cout << "Потный Гарри" << std::endl;
-	std::cout << "Хогвартс" << std::endl;
-	std::cout << "Мяч" << std::endl;
-	std::cout << "Хокей" << std::endl;
-	std::cout << "Красная Машина" << std::endl;
-	std::cout << "феррари" << std::endl;
-	std::cout << "Конь" << std::endl;
+   cout <<" Hello world!"<<endl;
 }
