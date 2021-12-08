11bc2ac8528a799f2cb91d3ca2f3a36bc50e94e6
Infendum <69817375+Infendum@users.noreply.github.com>
2021-12-08 20:48:22 +0600

    Update magister.cpp

--- magister.cpp
+++ magister.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-using std::cout;
-using std::endl;
+#include <Windows.h>
+#include <string>
 
-int main() {
-   cout <<" Hello world!"<<endl;
+
+
+void greetings (int npc_id ) {
+	if (npc_id == 1100) {
+		std::cout << "Здравствуй странник" << std::endl;
+	}
+	else if (npc_id == 1200) {
+		std::cout << "Я хочу забрать твои деньги" << std::endl;
+	}
+	if (npc_id == 1300) {
+		std::
+	}
 }
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	greetings(1200);
+	
+	
+}
+
